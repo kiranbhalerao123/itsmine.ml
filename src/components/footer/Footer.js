7978622dbaaf34aaf13bc3f9f5d09ced4cbcc78c
kiranbhalerao123 <bhaleraok65@gmail.com>
2018-12-09 16:45:31 +0530

    linkedin link added to footer text

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,8 +3,7 @@ class Footer extends Component {
   render() {
     return (
       <div
-        className="d-flex flex-column align-items-center pt-4 pb-4 w-100"
-        style={{ background: "#fff" }}
+        className="d-flex flex-column align-items-center pt-4 pb-4 w-100 bg-white"
         id="footer"
       >
         <p className="mb-0 pb-0">
@@ -12,7 +11,15 @@ class Footer extends Component {
           <strong>Open Source</strong>
         </p>
         <p className="text-center pl-3 pr-3 pt-1">
-          Copyright © 2018 Created by <strong>Kiran Bhalerao</strong>.
+          Copyright © 2018 Created by{" "}
+          <a
+            href="https://www.linkedin.com/in/kiran-bhalerao-b12aa9132/"
+            target="blank"
+            className="text-dark"
+          >
+            <strong>Kiran Bhalerao</strong>
+          </a>
+          .
         </p>
       </div>
     );
