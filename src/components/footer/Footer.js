ddaccc9c66fb751501d4b5f68cdcbd087f3efacb
kiranbhalerao123 <bhaleraok65@gmail.com>
2019-01-25 08:44:33 +0530

    added dynamic footer date

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,36 +1,36 @@
-import React, { Component } from "react";
-import "./Footer.css"
+import React, { Component } from 'react'
+import './Footer.css'
 class Footer extends Component {
   render() {
     return (
       <div
-        className="d-flex flex-column align-items-center pt-4 pb-4 w-100 bg-white"
-        id="footer"
+        className='d-flex flex-column align-items-center pt-4 pb-4 w-100 bg-white'
+        id='footer'
       >
-        <p className="mb-0 pb-0">
-          <span className="text-danger">{"<3"}</span>{" "}
+        <p className='mb-0 pb-0'>
+          <span className='text-danger'>{'<3'}</span>{' '}
           <strong>Open Source 😍</strong>
         </p>
-        <p className="text-center pl-3 pr-3 pt-1">
-          Copyright © 2019 Created by{" "}
+        <p className='text-center pl-3 pr-3 pt-1'>
+          Copyright © {new Date().getFullYear()} Created by{' '}
           <a
-            className="text-dark"
-            href="https://www.linkedin.com/in/kiran-bhalerao-b12aa9132/"
-            target="blank"
+            className='text-dark'
+            href='https://www.linkedin.com/in/kiran-bhalerao-b12aa9132/'
+            target='blank'
           >
             <strong>Kiran Bhalerao </strong>
           </a>
           Graphics by
           <a
-            className="text-dark"
-            href="https://www.freepik.com/free-vector/app-development-concept-with-flat-design_2533377.htm"
-            target="blank"
+            className='text-dark'
+            href='https://www.freepik.com/free-vector/app-development-concept-with-flat-design_2533377.htm'
+            target='blank'
           >
             <strong> Freepik</strong>
           </a>
         </p>
       </div>
-    );
+    )
   }
 }
-export default Footer;
+export default Footer
